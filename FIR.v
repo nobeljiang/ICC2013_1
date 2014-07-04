f6f819b1be1873ad8ece809e2d0d1cd788750713
nobeljiang <jwilove0049@gmail.com>
2014-07-04 18:38:30 +0800

    get some correct result now.

--- FIR.v
+++ FIR.v
@@ -46,61 +46,77 @@ reg  signed [15:0] data_shift_29;
 reg  signed [15:0] data_shift_30;
 reg  signed [15:0] data_shift_31;
 
-reg  signed [15:0] adder_00;
-reg  signed [15:0] adder_01;
-reg  signed [15:0] adder_02;
-reg  signed [15:0] adder_03;
-reg  signed [15:0] adder_04;
-reg  signed [15:0] adder_05;
-reg  signed [15:0] adder_06;
-reg  signed [15:0] adder_07;
-reg  signed [15:0] adder_08;
-reg  signed [15:0] adder_09;
-reg  signed [15:0] adder_10;
-reg  signed [15:0] adder_11;
-reg  signed [15:0] adder_12;
-reg  signed [15:0] adder_13;
-reg  signed [15:0] adder_14;
-reg  signed [15:0] adder_15;
+reg  signed [16:0] adder_00;
+reg  signed [16:0] adder_01;
+reg  signed [16:0] adder_02;
+reg  signed [16:0] adder_03;
+reg  signed [16:0] adder_04;
+reg  signed [16:0] adder_05;
+reg  signed [16:0] adder_06;
+reg  signed [16:0] adder_07;
+reg  signed [16:0] adder_08;
+reg  signed [16:0] adder_09;
+reg  signed [16:0] adder_10;
+reg  signed [16:0] adder_11;
+reg  signed [16:0] adder_12;
+reg  signed [16:0] adder_13;
+reg  signed [16:0] adder_14;
+reg  signed [16:0] adder_15;
 
-reg  signed [15:0] mult_00;
-reg  signed [15:0] mult_01;
-reg  signed [15:0] mult_02;
-reg  signed [15:0] mult_03;
-reg  signed [15:0] mult_04;
-reg  signed [15:0] mult_05;
-reg  signed [15:0] mult_06;
-reg  signed [15:0] mult_07;
-reg  signed [15:0] mult_08;
-reg  signed [15:0] mult_09;
-reg  signed [15:0] mult_10;
-reg  signed [15:0] mult_11;
-reg  signed [15:0] mult_12;
-reg  signed [15:0] mult_13;
-reg  signed [15:0] mult_14;
-reg  signed [15:0] mult_15;
+reg  signed [36:0] mult_00;
+reg  signed [36:0] mult_01;
+reg  signed [36:0] mult_02;
+reg  signed [36:0] mult_03;
+reg  signed [36:0] mult_04;
+reg  signed [36:0] mult_05;
+reg  signed [36:0] mult_06;
+reg  signed [36:0] mult_07;
+reg  signed [36:0] mult_08;
+reg  signed [36:0] mult_09;
+reg  signed [36:0] mult_10;
+reg  signed [36:0] mult_11;
+reg  signed [36:0] mult_12;
+reg  signed [36:0] mult_13;
+reg  signed [36:0] mult_14;
+reg  signed [36:0] mult_15;
 
+reg  sign_00;
+reg  sign_01;
+reg  sign_02;
+reg  sign_03;
+reg  sign_04;
+reg  sign_05;
+reg  sign_06;
+reg  sign_07;
+reg  sign_08;
+reg  sign_09;
+reg  sign_10;
+reg  sign_11;
+reg  sign_12;
+reg  sign_13;
+reg  sign_14;
+reg  sign_15;
 // Adder first level
-reg signed [15:0] adder_level1_00;
-reg signed [15:0] adder_level1_01;
-reg signed [15:0] adder_level1_02;
-reg signed [15:0] adder_level1_03;
-reg signed [15:0] adder_level1_04;
-reg signed [15:0] adder_level1_05;
-reg signed [15:0] adder_level1_06;
-reg signed [15:0] adder_level1_07;
+reg signed [37:0] adder_level1_00;
+reg signed [37:0] adder_level1_01;
+reg signed [37:0] adder_level1_02;
+reg signed [37:0] adder_level1_03;
+reg signed [37:0] adder_level1_04;
+reg signed [37:0] adder_level1_05;
+reg signed [37:0] adder_level1_06;
+reg signed [37:0] adder_level1_07;
 
 always@(posedge clk or posedge rst)
 	if(rst)
 	begin
-		adder_level1_00 <= 16'd0;
-		adder_level1_01 <= 16'd0;
-		adder_level1_02 <= 16'd0;
-		adder_level1_03 <= 16'd0;
-		adder_level1_04 <= 16'd0;
-		adder_level1_05 <= 16'd0;
-		adder_level1_06 <= 16'd0;
-		adder_level1_07 <= 16'd0;
+		adder_level1_00 <= 0;
+		adder_level1_01 <= 0;
+		adder_level1_02 <= 0;
+		adder_level1_03 <= 0;
+		adder_level1_04 <= 0;
+		adder_level1_05 <= 0;
+		adder_level1_06 <= 0;
+		adder_level1_07 <= 0;
 	end
 	else
 	begin
@@ -115,18 +131,18 @@ always@(posedge clk or posedge rst)
 	end
 
 // Adder second level
-reg signed [15:0] adder_level2_00;
-reg signed [15:0] adder_level2_01;
-reg signed [15:0] adder_level2_02;
-reg signed [15:0] adder_level2_03;
+reg signed [38:0] adder_level2_00;
+reg signed [38:0] adder_level2_01;
+reg signed [38:0] adder_level2_02;
+reg signed [38:0] adder_level2_03;
 
 always@(posedge clk or posedge rst)
 	if(rst)
 	begin
-		adder_level2_00 <= 16'd0;
-		adder_level2_01 <= 16'd0;
-		adder_level2_02 <= 16'd0;
-		adder_level2_03 <= 16'd0;
+		adder_level2_00 <= 0;
+		adder_level2_01 <= 0;
+		adder_level2_02 <= 0;
+		adder_level2_03 <= 0;
 	end
 	else
 	begin
@@ -137,14 +153,14 @@ always@(posedge clk or posedge rst)
 	end
 
 // Adder third level
-reg signed [15:0] adder_level3_00;
-reg signed [15:0] adder_level3_01;
+reg signed [39:0] adder_level3_00;
+reg signed [39:0] adder_level3_01;
 
 always@(posedge clk or posedge rst)
 	if(rst)
 	begin
-		adder_level3_00 <= 16'd0;
-		adder_level3_01 <= 16'd0;
+		adder_level3_00 <= 0;
+		adder_level3_01 <= 0;
 	end
 	else
 	begin
@@ -152,19 +168,36 @@ always@(posedge clk or posedge rst)
 		adder_level3_01 <= adder_level2_02 + adder_level2_03; 
 	end
 
-reg signed [15:0] adder_final;
+reg signed [40:0] adder_final;
 always@(posedge clk or posedge rst)
 	if(rst)
-		adder_final <= 16'd0;
+		adder_final <= 0;
 	else
 		adder_final <= adder_level3_00 + adder_level3_01;
 
 //assign fir_d = {adder_final[39], adder_final[14:0]};
-assign fir_d = adder_final;
+assign fir_d = {adder_final[40], adder_final[30:16]};
 
 always@(posedge clk or posedge rst)
 	if(rst)
 	begin
+		sign_00 <= 0;
+		sign_01 <= 0;
+		sign_02 <= 0;
+		sign_03 <= 0;
+		sign_04 <= 0;
+		sign_05 <= 0;
+		sign_06 <= 0;
+		sign_07 <= 0;
+		sign_08 <= 0;
+		sign_09 <= 0;
+		sign_10 <= 0;
+		sign_11 <= 0;
+		sign_12 <= 0;
+		sign_13 <= 0;
+		sign_14 <= 0;
+		sign_15 <= 0;
+
 		mult_00 <= 0;
 		mult_01 <= 0;
 		mult_02 <= 0;
@@ -184,22 +217,39 @@ always@(posedge clk or posedge rst)
 	end
 	else
 	begin
-		mult_00 <= adder_00 * FIR_C00;
-		mult_01 <= adder_01 * FIR_C01;
-		mult_02 <= adder_02 * FIR_C02;
-		mult_03 <= adder_03 * FIR_C03;
-		mult_04 <= adder_04 * FIR_C04;
-		mult_05 <= adder_05 * FIR_C05;
-		mult_06 <= adder_06 * FIR_C06;
-		mult_07 <= adder_07 * FIR_C07;
-		mult_08 <= adder_08 * FIR_C08;
-		mult_09 <= adder_09 * FIR_C09;
-		mult_10 <= adder_10 * FIR_C10;
-		mult_11 <= adder_11 * FIR_C11;
-		mult_12 <= adder_12 * FIR_C12;
-		mult_13 <= adder_13 * FIR_C13;
-		mult_14 <= adder_14 * FIR_C14;
-		mult_15 <= adder_15 * FIR_C15;
+		sign_00 <= FIR_C00[19] ^ adder_00[16];
+		sign_01 <= FIR_C01[19] ^ adder_01[16];
+		sign_02 <= FIR_C02[19] ^ adder_02[16];
+		sign_03 <= FIR_C03[19] ^ adder_03[16];
+		sign_04 <= FIR_C04[19] ^ adder_04[16];
+		sign_05 <= FIR_C05[19] ^ adder_05[16];
+		sign_06 <= FIR_C06[19] ^ adder_06[16];
+		sign_07 <= FIR_C07[19] ^ adder_07[16];
+		sign_08 <= FIR_C08[19] ^ adder_08[16];
+		sign_09 <= FIR_C09[19] ^ adder_09[16];
+		sign_10 <= FIR_C10[19] ^ adder_10[16];
+		sign_11 <= FIR_C11[19] ^ adder_11[16];
+		sign_12 <= FIR_C12[19] ^ adder_12[16];
+		sign_13 <= FIR_C13[19] ^ adder_13[16];
+		sign_14 <= FIR_C14[19] ^ adder_14[16];
+		sign_15 <= FIR_C15[19] ^ adder_15[16];
+
+		mult_00 <= FIR_C00 * adder_00;
+		mult_01 <= FIR_C01 * adder_01;
+		mult_02 <= FIR_C02 * adder_02;
+		mult_03 <= FIR_C03 * adder_03;
+		mult_04 <= FIR_C04 * adder_04;
+		mult_05 <= FIR_C05 * adder_05;
+		mult_06 <= FIR_C06 * adder_06;
+		mult_07 <= FIR_C07 * adder_07;
+		mult_08 <= FIR_C08 * adder_08;
+		mult_09 <= FIR_C09 * adder_09;
+		mult_10 <= FIR_C10 * adder_10;
+		mult_11 <= FIR_C11 * adder_11;
+		mult_12 <= FIR_C12 * adder_12;
+		mult_13 <= FIR_C13 * adder_13;
+		mult_14 <= FIR_C14 * adder_14;
+		mult_15 <= FIR_C15 * adder_15;
 	end
 
 always@(posedge clk or posedge rst)
@@ -329,20 +379,41 @@ always@(posedge clk or posedge rst)
 		      data_shift_06 * FIR_C07 + data_shift_05 * FIR_C06 + data_shift_04 * FIR_C05 + data_shift_03 * FIR_C04 +
 		      data_shift_02 * FIR_C03 + data_shift_01 * FIR_C02 + data_shift_00 * FIR_C01 + data * FIR_C00 ;
 
+reg data_valid_dly;
+wire data_valid_start;
+wire data_valid_end;
+
+assign	data_valid_start = data_valid && !data_valid_dly;
+assign 	data_valid_end = data_valid_dly && !data_valid;
+
+always@(posedge clk or posedge rst)
+	if(rst)
+		data_valid_dly <= 0;
+	else
+		data_valid_dly <= data_valid;
+
 always@(posedge clk or posedge rst)
 	if(rst)
 		fir_counter <= 6'd0;
-	else if(data_valid)
+	else if(|fir_counter)
 		fir_counter <= fir_counter + 6'd1;
-	else
-		fir_counter <= 6'd0;
+	else if(data_valid_start || data_valid_end)
+		fir_counter <= 1;
 
 always@(posedge clk or posedge rst)
 	if(rst)
 		fir_valid <= 1'b0;
-	else if(fir_counter == 6'd0)
+	else if((fir_counter == 6'd37) && !data_valid)
 		fir_valid <= 1'b0;
-	else if(fir_counter == 6'd37)
+	else if((fir_counter == 6'd37) && data_valid)
 		fir_valid <= 1'b1;
 
+reg [10:0] fir_cnt;
+always@(posedge clk or posedge rst)
+	if(rst)
+		fir_cnt <= 0;
+	else if(|fir_cnt)
+		fir_cnt <= fir_cnt + 1;
+	else if(fir_valid)
+		fir_cnt <= 1;
 endmodule
