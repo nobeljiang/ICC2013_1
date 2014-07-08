aa11f410d111a436fbae0ea80805bf6802515519
nobeljiang <jwilove0049@gmail.com>
2014-07-08 16:24:09 +0800

    add FFT function

--- FAS.v
+++ FAS.v
@@ -1,3 +1,6 @@
+
+
+
 module  FAS (data_valid, data, clk, rst, fir_d, fir_valid, fft_valid, done, freq,
  fft_d1, fft_d2, fft_d3, fft_d4, fft_d5, fft_d6, fft_d7, fft_d8,
  fft_d9, fft_d10, fft_d11, fft_d12, fft_d13, fft_d14, fft_d15, fft_d0);
@@ -15,6 +18,13 @@ output [3:0] freq;
 FIR FIR_0(.clk(clk), .rst(rst), .data_valid(data_valid), .data(data),
 	.fir_d(fir_d), .fir_valid(fir_valid));
 
+
+FFT FFT_0(.clk(clk), .rst(rst), .fir_d(fir_d), .fir_valid(fir_valid),
+	.fft_d0(fft_d0), .fft_d1(fft_d1), .fft_d2(fft_d2), .fft_d3(fft_d3),  
+	.fft_d4(fft_d4), .fft_d5(fft_d5), .fft_d6(fft_d6), .fft_d7(fft_d7),  
+	.fft_d8(fft_d8), .fft_d9(fft_d9), .fft_d10(fft_d10), .fft_d11(fft_d11),  
+	.fft_d12(fft_d12), .fft_d13(fft_d13), .fft_d14(fft_d14), .fft_d15(fft_d15),
+	.fft_valid(fft_valid));
 endmodule
 
 
