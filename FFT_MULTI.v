bfb1bd3fd294c2127055e383244ae494ce45bc09
nobeljiang <jwilove0049@gmail.com>
2014-07-08 17:18:38 +0800

    Update FFT_MULTI.v

--- FFT_MULTI.v
+++ FFT_MULTI.v
@@ -14,16 +14,24 @@ wire signed [31:0] b_d_sub = imag_b - imag_a;
 reg signed [31:0] real_out;
 reg signed [31:0] imag_out;
 
+wire signed [63:0] mult_tmp1 = a_c_sub*Real_coff;
+wire signed [63:0] mult_tmp2 = d_b_sub*Imag_coff;
+wire signed [63:0] mult_tmp3 = a_c_sub*Imag_coff;
+wire signed [63:0] mult_tmp4 = b_d_sub*Real_coff;
+
+wire signed [63:0] adder_tmp1 = mult_tmp1 + mult_tmp2;
+wire signed [63:0] adder_tmp2 = mult_tmp3 + mult_tmp4;
+
 always@(posedge clk or posedge rst)
 	if(rst)
 		real_out <= 0;
 	else if(valid) 
-		real_out <= a_c_sub*Real_coff + d_b_sub*Imag_coff;
+		real_out <= {adder_tmp1[63], adder_tmp1[46:16]} ;
 
 always@(posedge clk or posedge rst)
 	if(rst)
 		imag_out <= 0;
 	else if(valid) 
-		imag_out <= a_c_sub*Imag_coff + b_d_sub*Real_coff;
+		imag_out <= {adder_tmp2[63], adder_tmp2[46:16]};
 
 endmodule
