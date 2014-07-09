303ed1c21d2f131e5be970a0e4ee0f9ed1e9e414
nobeljiang <jwilove0049@gmail.com>
2014-07-09 17:37:17 +0800

    Update FAS.v

--- FAS.v
+++ FAS.v
@@ -25,6 +25,13 @@ FFT FFT_0(.clk(clk), .rst(rst), .fir_d(fir_d), .fir_valid(fir_valid),
 	.fft_d8(fft_d8), .fft_d9(fft_d9), .fft_d10(fft_d10), .fft_d11(fft_d11),  
 	.fft_d12(fft_d12), .fft_d13(fft_d13), .fft_d14(fft_d14), .fft_d15(fft_d15),
 	.fft_valid(fft_valid));
+
+Analysis Analysis_0(.clk(clk), .rst(rst), 
+	.fft_d0(fft_d0), .fft_d1(fft_d1), .fft_d2(fft_d2), .fft_d3(fft_d3),  
+	.fft_d4(fft_d4), .fft_d5(fft_d5), .fft_d6(fft_d6), .fft_d7(fft_d7),  
+	.fft_d8(fft_d8), .fft_d9(fft_d9), .fft_d10(fft_d10), .fft_d11(fft_d11),  
+	.fft_d12(fft_d12), .fft_d13(fft_d13), .fft_d14(fft_d14), .fft_d15(fft_d15),
+	.fft_valid(fft_valid), .done(done), .freq(freq));
 endmodule
 
 
